a8f8e3a6221c1caf63580dbe2213ceda8fc56511
Bruno Braga <bruno.braga@ufms.br>
2021-11-11 14:44:07 -0400

    Update Multidock_Vna.py

--- Multidock_Vna.py
+++ Multidock_Vna.py
@@ -2,42 +2,49 @@ import random
 
 random.seed()
 
-#'python3 verify_log.py Log_R%d.out'%i
-newpy = open('verify_log.py','w')
-newpy.write('if __name__ == "__main__":\n')
-newpy.write('\timport sys\n')
-newpy.write('\tfrom os import system as cmd\n')
-newpy.write('\targ = sys.argv\n')
-newpy.write('\tif len(arg) == 2 and "log" in arg[1].lower():\n')
-newpy.write('\t\ta1 = arg[1].find("_R")+2\n')
-newpy.write('\t\ta2 = arg[1].find(".out")\n')
-newpy.write('\t\tinp_file = "config4vina_%s.inp"%(arg[1][a1:a2])\n')###
-newpy.write('\t\tg = open(inp_file,"r")\n')
-newpy.write('\t\tinp_lines = g.readlines()\n')
-newpy.write('\t\tg.close()\n')
-newpy.write('\t\tfor temp in inp_lines:\n')
-newpy.write('\t\t\tif "out" in temp:\n')
-newpy.write('\t\t\t\tdeez = temp.split()\n')
-newpy.write('\t\t\t\tout_file = deez[len(deez)-1]\n')###
-newpy.write('\t\t\t\tbreak\n')
-newpy.write('\t\tf = open(arg[1],"r")\n')
-newpy.write('\t\tflist = f.readlines()\n')
-newpy.write('\t\tf.close()\n')
-newpy.write('\t\tfor i in flist:\n')
-newpy.write('\t\t\tdata = i.split()\n')
-newpy.write('\t\t\tif len(data) == 4 and "1" in data[0]:\n')
-newpy.write('\t\t\t\tbreak\n')
-newpy.write('\t\tif float(data[1]) >=0:\n') # affinity >= 0 kcal/mol :: bad docking
-newpy.write('\t\t\tcmd("mv %s Useless_dock"%(out_file))\n')
-newpy.write('\t\t\tcmd("mv %s Useless_dock"%(arg[1]))\n')
-newpy.write('\t\telif float(data[1]) >=-4.5:\n') # 0 > affinity >= -4.5 kcal/mol :: decent docking
-newpy.write('\t\t\tcmd("mv %s Ok_dock"%(out_file))\n')
-newpy.write('\t\t\tcmd("mv %s Ok_dock"%(arg[1]))\n')
-newpy.write('\t\telse:\n') # -4.5 kcal/mol > affinity  :: good docking
-newpy.write('\t\t\tcmd("mv %s Nice_dock"%(out_file))\n')
-newpy.write('\t\t\tcmd("mv %s Nice_dock"%(arg[1]))\n')
-newpy.write('\t\tcmd("rm %s"%(inp_file))\n')
-newpy.close()
+def filtro_log(pdb=False):
+	newpy = open('verify_log.py','w')
+	newpy.write('if __name__ == "__main__":\n')
+	newpy.write('\timport sys\n')
+	newpy.write('\tfrom os import system as cmd\n')
+	newpy.write('\targ = sys.argv\n')
+	newpy.write('\tif len(arg) == 2 and "log" in arg[1].lower():\n')
+	newpy.write('\t\ta1 = arg[1].find("_R")+2\n')
+	newpy.write('\t\ta2 = arg[1].find(".out")\n')
+	newpy.write('\t\tinp_file = "config4vina_%s.inp"%(arg[1][a1:a2])\n')###
+	newpy.write('\t\tg = open(inp_file,"r")\n')
+	newpy.write('\t\tinp_lines = g.readlines()\n')
+	newpy.write('\t\tg.close()\n')
+	newpy.write('\t\tfor temp in inp_lines:\n')
+	newpy.write('\t\t\tif "out" in temp:\n')
+	newpy.write('\t\t\t\tdeez = temp.split()\n')
+	newpy.write('\t\t\t\tout_file = deez[len(deez)-1]\n')###
+	if pdb:
+		newpy.write('\t\t\t\tpdb_format = "%s.pdb"%(out_file[:-6])\n')
+	newpy.write('\t\t\t\tbreak\n')
+	newpy.write('\t\tf = open(arg[1],"r")\n')
+	newpy.write('\t\tflist = f.readlines()\n')
+	newpy.write('\t\tf.close()\n')
+	newpy.write('\t\tfor i in flist:\n')
+	newpy.write('\t\t\tdata = i.split()\n')
+	newpy.write('\t\t\tif len(data) == 4 and "1" in data[0]:\n')
+	newpy.write('\t\t\t\tbreak\n')
+	newpy.write('\t\tfinal_out = out_file\n')
+	if pdb:
+		newpy.write('\t\tcmd("obabel -i pdbqt %s -o pdb -O %s"%(out_file, pdb_format))\n')
+		newpy.write('\t\tfinal_out = pdb_format\n')
+		newpy.write('\t\t\tcmd("rm %s"%(out_file))\n')
+	newpy.write('\t\tif float(data[1]) >=0:\n') # affinity >= 0 kcal/mol :: bad docking
+	newpy.write('\t\t\tcmd("mv %s Useless_dock"%(final_out))\n')
+	newpy.write('\t\t\tcmd("mv %s Useless_dock"%(arg[1]))\n')
+	newpy.write('\t\telif float(data[1]) >=-4.5:\n') # 0 > affinity >= -4.5 kcal/mol :: decent docking
+	newpy.write('\t\t\tcmd("mv %s Ok_dock"%(final_out))\n')
+	newpy.write('\t\t\tcmd("mv %s Ok_dock"%(arg[1]))\n')
+	newpy.write('\t\telse:\n') # -4.5 kcal/mol > affinity  :: good docking
+	newpy.write('\t\t\tcmd("mv %s Nice_dock"%(final_out))\n')
+	newpy.write('\t\t\tcmd("mv %s Nice_dock"%(arg[1]))\n')
+	newpy.write('\t\tcmd("rm %s"%(inp_file))\n')
+	newpy.close()
 
 def multi_inp(cent_x = -18.287, cent_y = -9.034, cent_z = 7.361, max_dv = 5,
 			  recpt = '6eqe.pdbqt', ligand = 'C8X.pdbqt', out_na = 'docked_C8X.pdbqt',
@@ -94,13 +101,15 @@ if __name__ == "__main__":
 	cent_z    = 7.361
 	recpt     = '' #'6eqe.pdbqt'
 	ligand    = '' #'C8X_BHET.pdbqt'
+	new_out   = False
+	convert   = False
 	out_na    = ''
 	max_dv    = 5
 	multi     = 10
 	vina_home = '~/autodock_vina_1_1_2_linux_x86/bin'
 	inst_only = False
 
-	flags = ["&","-h","--help",'-vnh','-e','-dbox','-dcenter','-maxdev','-mult','-recpt','-lig','-out']
+	flags = ["&","-h","--help",'-vnh','-pdbqt2pdb','-e','-dbox','-dcenter','-maxdev','-mult','-recpt','-lig','-out']
 
 	# Flag verification
 	for i in arg:
@@ -122,13 +131,14 @@ if __name__ == "__main__":
 			print("\t-vnh\t\tVina home directory.\n")
 			print("\t-recpt\t\tReceptor file.\n\t-lig\t\tLigand file.\n")
 			print("\t-out\t\tName of the pdbqt output file with all models vina propose. (Default: Docked_YOURLIGAND.pdbqt).\n")
+			print("\t-pdbqt2pdb\t\t(If openBabel is installed) Converts the PDBQT format of vina output to PDB with obabel.\n")
 			print("\t-dcenter\t\tXYZ-Coordinates of the center of the gridbox for docking.\n")
 			print("\t-dbox\t\tDimensions XYZ of the gridbox.\n")
 			print("\t-e\t\tExhaustiveness used. Default: 10.\n")
 			print("\t-maxdev\t\tMaximum deviation from the gridbox center, for the random multiple random choices of gridboxes. Default: 5 angstrom.\n")
 			print("\t-mult\t\tNumber of multiple docking tries. Default: 10.\n")
 			print("\nExamples:\n\t$ python3 Multidock_Vna.py -recpt 6eqe.pdbqt -lig C8X_BHET.pdbqt\n")
-			print("\n\t$ python3 Multidock_Vna.py -recpt 6eqe.pdbqt -lig C8X_BHET.pdbqt -out docked_C8X.pdbqt -vnh -dcenter -18.287 -9.034 7.361\n\t\tObs: '-vnh' used without any information if vina on your PC PATH\n")
+			print("\n\t$ python3 Multidock_Vna.py -recpt 6eqe.pdbqt -lig C8X_BHET.pdbqt -out docked_C8X.pdbqt -pdbqt2pdb -vnh -dcenter -18.287 -9.034 7.361\n\t\tObs: '-vnh' used without any information if vina on your PC PATH\n")
 			print("\n\t$ python3 Multidock_Vna.py -recpt 6eqe.pdbqt -lig C8X_BHET.pdbqt -out docked_C8X.pdbqt -dcenter -18.287 -9.034 7.361 -e 50 -maxdev 2.5 -mult 100\n")
 			break
 
@@ -139,13 +149,18 @@ if __name__ == "__main__":
 		elif arg[i].lower() == '-vnh':
 			i+=1
 			vina_home = arg[i]
+		elif arg[i].lower() == '-pdbqt2pdb':
+			i+=1
+			convert = True
 		elif arg[i].lower() == '-lig':
 			i+=1
 			ligand = arg[i]
-			out_na = 'Docked_%s'%ligand
+			if not new_out:
+				out_na = 'Docked_%s'%ligand
 		elif arg[i].lower() == '-out':
 			i+=1
-			out_na = arg[i]
+			new_out = True
+			out_na  = arg[i]
 		elif arg[i].lower() == '-dcenter':
 			cent_x = float(arg[i+1])
 			cent_y = float(arg[i+2])
@@ -170,6 +185,7 @@ if __name__ == "__main__":
 	if not inst_only:
 		if '' not in [recpt, ligand]:
 			multi_inp(cent_x=cent_x,cent_y=cent_y,cent_z=cent_z,max_dv=max_dv,recpt=recpt, ligand=ligand, out_na=out_na,box_x=box_x,box_y=box_y,box_z=box_z,exh=exh,vina_home=vina_home)
+			filtro_log(pdb=convert)
 			cmd('sh exec_vina.sh')
 		else:
 			print('Receptor and/or ligand file not chosen!')
